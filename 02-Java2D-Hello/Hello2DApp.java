25aa9235132b536e0aa715b662d20d93ce58c245
Victor Milhomem <64241639+VictorMilhomem@users.noreply.github.com>
2021-08-09 09:59:42 -0300

    Add files via upload

---
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Hello2DApp {
+    public static void main (String[] args) {
+        Hello2DFrame frame = new Hello2DFrame();
+    }
+}
+
+class Hello2DFrame extends JFrame {
+    public Hello2DFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java2D - Hello World!");
+        this.setSize(350, 350);
+        this.setVisible(true);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setBackground(Color.white);
+        g2d.clearRect(0, 0, 350, 350);
+
+        g2d.setPaint(Color.black);
+        int w = getWidth();
+        int h = getHeight();
+        g2d.drawLine(0,0, w,h);
+        g2d.drawLine(0,h, w,0);
+
+        g2d.setColor(Color.RED);
+        g2d.drawRect((w/2) - 50, 50, 70, 50);
+        g2d.fillRect((w/2) - 50, 50, 70, 50);
+
+        g2d.setColor(Color.blue);
+        char[] texto = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+        g2d.drawChars(texto, 0, 12, (w/2) - 50, h - 100);
+    }
+}
