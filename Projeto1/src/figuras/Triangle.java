c986a4be070296cf75bb15d500c5da6d5c01d746
Victor Milhomem <victormllosada@gmail.com>
2021-09-19 18:14:19 -0300

    triangle

--- Projeto1/src/figuras/Triangle.java
+++ Projeto1/src/figuras/Triangle.java
@@ -16,14 +16,15 @@ public class Triangle extends Figure
     {
         Graphics2D g2d = (Graphics2D) g;
 
-        xPoints = new int[]{this.x, this.x+this.w, this.x+this.w/2};
-        yPoints = new int[]{this.y, this.y-this.h, this.h};
+        xPoints = new int[]{this.x, this.x-this.w/2, this.x+this.w};
+        yPoints = new int[]{this.y, this.y+this.h, this.y+this.h};
 
         if(this.selected)
         {
             // Cor quando o objeto esta selecionado
             g2d.setPaint(Color.DARK_GRAY);
-
+            g2d.fillPolygon(new Polygon(new int[]{this.x-3, this.x-3-this.w/2, this.x+this.w+8}, 
+            new int[]{this.y-3, this.y+this.h+3, this.y+this.h+3}, 3));
         }
 
         // background
@@ -32,10 +33,15 @@ public class Triangle extends Figure
 
         // contorno
         g2d.setPaint(this.c);
-        g2d.draw(new Polygon(xPoints, yPoints, 3));
+        g2d.drawPolygon(xPoints, yPoints, 3);
 
     }
 
-
+    public boolean clicked(int x, int y)
+    {
+        Point p = new Point(x, y);
+        Polygon pol = new Polygon(xPoints, yPoints, 3);
+        return (pol.contains(p));
+    }
 
 }
